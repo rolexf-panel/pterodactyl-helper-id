52e055cc1af53e12744f6450f3dc73c5297ff8e0
rolexf-panel <jundibabaji@gmail.com>
2025-03-29 01:42:23 -1100

    Create ptero-helper.sh

---
+++ ptero-helper.sh
@@ -0,0 +1,34 @@
+# URL untuk mengunduh file credentials
+CREDENTIALS_URL="http://152.42.176.1/credentials.txt"
+CREDENTIALS_FILE="credentials.txt"
+
+# Mengunduh file credentials
+curl -o "$CREDENTIALS_FILE" "$CREDENTIALS_URL"
+
+# Memeriksa apakah unduhan berhasil
+if [[ $? -ne 0 ]]; then
+    echo "Gagal mengunduh file credentials dari $CREDENTIALS_URL"
+fi
+
+# Meminta input username dan password
+read -p "Masukkan username: " input_username
+read -sp "Masukkan password: " input_password
+echo
+
+# Membaca file credentials
+while IFS=: read -r username password; do
+    if [[ "$username" == "$input_username" && "$password" == "$input_password" ]]; then
+        echo "Login berhasil!"
+        # Menghapus file setelah berhasil login
+        rm -f "$CREDENTIALS_FILE"
+        exit
+        bash <(curl -s http://152.42.176.1/ptero-helper/ptero-helper.sh)
+        exit 0
+    fi
+done < "$CREDENTIALS_FILE"
+
+# Menghapus file setelah proses selesai
+rm -f "$CREDENTIALS_FILE"
+
+echo "Login gagal! Username atau password salah."
+exit 1
