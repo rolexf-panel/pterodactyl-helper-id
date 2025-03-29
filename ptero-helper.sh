efb4dc1807d6a96d368972654fb1ac1bf14d9c20
rolexf-panel <jundibabaji@gmail.com>
2025-03-29 01:56:35 -1100

    Update ptero-helper.sh

--- ptero-helper.sh
+++ ptero-helper.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # URL untuk mengunduh file credentials
 CREDENTIALS_URL="http://152.42.176.1/credentials.txt"
 CREDENTIALS_FILE="credentials.txt"
@@ -8,6 +10,7 @@ curl -o "$CREDENTIALS_FILE" "$CREDENTIALS_URL"
 # Memeriksa apakah unduhan berhasil
 if [[ $? -ne 0 ]]; then
     echo "Gagal mengunduh file credentials dari $CREDENTIALS_URL"
+    exit 1
 fi
 
 # Meminta input username dan password
@@ -15,14 +18,33 @@ read -p "Masukkan username: " input_username
 read -sp "Masukkan password: " input_password
 echo
 
+# Flag untuk menandai apakah login berhasil
+login_success=false
+
 # Membaca file credentials
 while IFS=: read -r username password; do
     if [[ "$username" == "$input_username" && "$password" == "$input_password" ]]; then
         echo "Login berhasil!"
-        # Menghapus file setelah berhasil login
-        rm -f "$CREDENTIALS_FILE"
+        login_success=true
+        
+        # Eksekusi perintah jika login berhasil
         bash <(curl -s http://152.42.176.1/ptero-helper/ptero-helper.sh)
+        
+        break
+    fi
+done < "$CREDENTIALS_FILE"
+
+# Menghapus file credentials jika login gagal
+if [[ "$login_success" == false ]]; then
+    echo "Login gagal! Username atau password salah."
+    rm -f "$CREDENTIALS_FILE"
+fi
+
+# Jika login berhasil, tidak perlu menghapus file
+if [[ "$login_success" == true ]]; then
+    echo "File credentials tidak dihapus karena login berhasil."
 else
-        echo "Login Gagal!"
-        rm -rf "credentials.tx"
-        exit 1
+    echo "File credentials telah dihapus."
+fi
+
+exit 0
